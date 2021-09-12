b6239d533c480c6dec26bfe7a73b0c8482ef6139
Youness Mouhajir <marialalij@gmail.com>
2021-09-13 01:13:31 +0200

    add comment

---
+++ src/Controller/CommentController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Comment;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\CommentType;
+
+class CommentController extends AbstractController
+{
+}
