e7fb2c388380cdd900ce8635926e0e216b8b63c1
Youness Mouhajir <marialalij@gmail.com>
2021-11-17 15:08:37 +0100

    quality

--- src/Form/ImageType.php
+++ src/Form/ImageType.php
@@ -1,3 +1,41 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ff0116720b53b938605f7ab60ed701d94fa9c2f3e9e8d2babce71de3feee702d
-size 1281
+<?php
+
+namespace App\Form;
+
+use App\Entity\Image;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Image as ImageConstraint;
+
+class ImageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+        $builder
+            ->add('file', FileType::class, [
+                'label' => false,
+                'required' => false,
+                'constraints' => [
+                    new ImageConstraint(),
+                ],
+            ])
+            ->add('delete', ButtonType::class, [
+                'label_html' => true,
+                'label' => '<i class="fas fa-times"></i>',
+                'attr' => [
+                    'data-action' => 'delete',
+                    'data-target' => '#trick_images___name__',
+                ],
+            ]);
+    }
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Image::class,
+        ]);
+    }
+}
